4a40b0f331224608d7b117d3a7dc0c2291bf36a8
Sinclair Hudson <hudson.sinclair00@gmail.com>
2022-02-14 21:19:16 -0500

    updated the introduction

--- src/Opener.js
+++ src/Opener.js
@@ -9,10 +9,11 @@ const pub = process.env.PUBLIC_URL;
 
 class Opener extends Component {
     render() {
-        let Opener= "Hello! My name is Sinclair, and I'm currently attending the University of Waterloo for" +
-            " Computer Science. I love learning new things, creating cool projects, and solving problems." +
-            " This website serves as my online portfolio, including my projects, work experience, and events" +
-            " that I have attended. Take a look around, and don't hesitate to contact me!";
+        let Opener= "Hello! My name is Sinclair, and I'm currently attending the University of Waterloo for Computer Science." +
+            " This website serves as my online portfolio, including my projects and  work experience." +
+            " Currently, I'm looking for my final coop placement, in the Fall 2022 term." +
+            " After that, I'll be looking for a full-time job, since I'm graduating in May 2023." +
+            " Hit me up if you have an opportunity, my contact info can be found under the 'Contact' tab";
         return (
             <div>
                         <h2>Hello! I'm</h2>
